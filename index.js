bdb71154b97e2555e1fc961f1d24c328b8308b2b
opensas <opensas@gmail.com>
2025-09-03 01:36:13 -0300

    fix multiplatform file concatenating

--- index.js
+++ index.js
@@ -1,46 +1,53 @@
-import { copyFileSync } from 'node:fs';
-import { fileURLToPath } from 'node:url';
-import node_adapter from '@sveltejs/adapter-node'
-import child_process from 'node:child_process';
+import node_adapter from "@sveltejs/adapter-node";
+import { join } from "node:path";
+import { copyFileSync } from "node:fs";
+import { fileURLToPath } from "node:url";
+import { execSync } from "node:child_process";
 
-
-const files = fileURLToPath(new URL('./files', import.meta.url).href);
+const files = fileURLToPath(new URL("./files", import.meta.url));
 
 /** @type {import('.').default} */
 export default function (opts = {}) {
-	const { out = 'build', precompress, envPrefix = '', polyfill = true, includePackage = true, buildNodeModules = false, transferEnv = false } = opts;
+  const {
+    out = "build",
+    precompress,
+    envPrefix = "",
+    polyfill = true,
+    includePackage = true,
+    buildNodeModules = false,
+    transferEnv = false,
+  } = opts;
 
   /** @type {import('@sveltejs/kit').Adapter} */
-  const na = node_adapter({out, precompress, envPrefix, polyfill});
+  const na = node_adapter({ out, precompress, envPrefix, polyfill });
 
-	return {
-		name: 'sveltekit-adapter-node-iis',
+  return {
+    name: "sveltekit-adapter-node-iis",
 
-		async adapt(builder) {
+    async adapt(builder) {
+      console.info("!!! v3*** Running local version using pnpm link!!!");
       console.info("Running @sveltejs/adapter-node");
       await na.adapt(builder);
       console.info("Finished @sveltejs/adapter-node");
 
-      console.info("Running sveltekit-adapter-node-iis");
+      console.info("Running @opensas/sveltekit-adapter-node-iis");
 
-      copyFileSync(`${files}\\server.cjs`, `${out}\\server.cjs`);
-      copyFileSync(`${files}\\web.config`, `${out}\\web.config`);
+      copyFileSync(join(files, "server.cjs"), join(out, "server.cjs"));
+      copyFileSync(join(files, "web.config"), join(out, "web.config"));
 
       if (includePackage) {
-        copyFileSync(`package.json`, `${out}\\package.json`);
-        copyFileSync(`package-lock.json`, `${out}\\package-lock.json`);
+        copyFileSync("package.json", join(out, "package.json"));
+        copyFileSync("package-lock.json", join(out, "package-lock.json"));
 
         if (buildNodeModules) {
-          console.info("Building node_modules")
-          child_process.execSync(`cd ${out} && npm ci --omit dev`,{stdio:[0,1,2]});
+          console.info("Building node_modules");
+          execSync(`cd ${out} && npm ci --omit dev`, { stdio: [0, 1, 2] });
         }
       }
 
-      if (transferEnv) {
-        copyFileSync(`.env`, `${out}\\.env`);
-      }
+      if (transferEnv) copyFileSync(".env", join(out, ".env"));
 
-      console.info("Finished sveltekit-adapter-node-iis");
-    }
-  }
+      console.info("Finished @opensas/sveltekit-adapter-node-iis");
+    },
+  };
 }
