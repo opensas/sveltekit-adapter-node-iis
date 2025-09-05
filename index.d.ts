043a3e14e0456ad91c8028b37fc8598688d48997
opensas <opensas@gmail.com>
2025-09-05 02:18:37 -0300

    update types

--- index.d.ts
+++ index.d.ts
@@ -10,14 +10,23 @@ interface AdapterOptions {
 	polyfill?: boolean;
 	/** Copies `package.json` and `package-lock.json` to the output directory. */
 	includePackage?: boolean;
-	/** Builds `node_modules` in the output directory. Asumes `includePackage` to be`true`. */
+	/** Builds `node_modules` in the output directory. Assumes `includePackage` to be`true`. */
 	buildNodeModules?: boolean;
 	/** Allows to override the command used to build `node_modules`. */
 	buildCommand?: string;
 	/** Package manager to use.Options`npm` `pnpm` `yarn` `bun` `deno` */
 	packageManager?: 'npm' | 'pnpm' | 'yarn' | 'bun' | 'deno';
-	/** Array of additional files to copy to the output directory (e.g., `["prisma/schema.prisma", ".env.production"]`). */
-	copyFiles?: string[]
+	/** Array of additional files or folders to copy to the output directory
+	 * e.g., `["prisma/schema.prisma", ".env.production"]`
+	 * or more complex cases
+	 * [
+	 *   '/config.env',             // single file → preserves structure:               config.env => build/config/.env
+	 *   'iis',                     // folder → copies all files, preserves structure:  iis/* => build/iis/*
+	 *   { src: 'iis', dest: '.' }, // folder → copied directly into output root:       iis/* => build/*
+	 *   { src: 'c/file.txt', dest: 'x/new.txt' } // file → renamed in output:          c/file.txt => build/x/new.txt
+	 * ]
+		*/
+	copyFiles?: Array<string | { src: string; dest?: string }>;
 }
 
 export default function plugin(options?: AdapterOptions): Adapter;
